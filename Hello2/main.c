a47dc2fee8fba7c23549eb203457e15d422309bb
Damian <Traveler@Damian.local>
2013-12-31 16:49:11 -0500

    Initial Commit

---
+++ Hello2/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Hello2
+//
+//  Created by Damian on 12/31/13.
+//  Copyright (c) 2013 Damian. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
